93596693ab5896e02f074a6825d9c4b441bf4ca9
fr1t2 <fr1t2@protonmail.com>
2021-02-28 08:22:20 -0700

    cleaup and re-format site

--- script/NewCMD.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# FIX-ME
-
-TEST_STATE='qrl --json --port_pub 19010 --host 127.0.0.1 state'
-NODE_STATE='qrl --json --port_pub 19009 --host 127.0.0.1 state'
-
-echo -e "testnet: \n`$TEST_STATE`" && echo -e "mainnet: \n`$NODE_STATE`"
--- script/listAddresses.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This script is to grab the address for donation on the main page
-
-# FIX-ME
-
-curl --silent -XGET http://127.0.0.1:5359/api/ListAddresses | jq '.addresses[0]' |cut -d '"' -f 2
