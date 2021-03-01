62297f17411816b6d8d9f495635dd9066897d908
fr1t2 <fr1t2@protonmail.com>
2021-02-28 17:55:48 -0700

    add chacksum file

---
+++ _site/script/start_qrl-testnet.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+## Start QRL node and all related functions in screen sessions to run the tipbot
+## Need to start the node first, then run qrl_walletd, then the proxy
+#
+## Run this script from crontab on @reboot
+#
+#
+source "/home/$USER/.profile"
+
+# uncomment hte next line for testnet,
+screen -Sdm testnetqrl start_qrl --network-type testnet
+#screen -Sdm qrl start_qrl
